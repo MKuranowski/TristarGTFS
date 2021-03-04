5aeca664499aee932a0c2c83ce497c2320151ce4
Mikołaj Kuranowski <mkuranowski@gmail.com>
2021-03-04 13:54:21 +0100

    linting

--- tristargtfs.py
+++ tristargtfs.py
@@ -1,20 +1,12 @@
-from contextlib import contextmanager
 from itertools import chain
-from warnings import warn
-from tempfile import TemporaryFile, NamedTemporaryFile
-from datetime import datetime, timedelta
-import pyroutelib3
+from tempfile import TemporaryFile
+from datetime import date, datetime, timedelta
 import argparse
+from typing import Any
 import requests
 import zipfile
-import signal
-import math
-import json
 import time
-import zlib
-import rdp
 import csv
-import re
 import io
 import os
 
@@ -23,24 +15,22 @@ __author__ = "Mikołaj Kuranowski"
 __email__ = "mikolaj@mkuran.pl"
 __license__ = "MIT"
 
-@contextmanager
-def time_limit(sec):
-    "Time limter based on https://gist.github.com/Rabbit52/7449101"
-    def handler(x, y): raise TimeoutError
-    signal.signal(signal.SIGALRM, handler)
-    signal.alarm(sec)
-    try: yield
-    finally: signal.alarm(0)
 
-def csv_escape(txt):
+def csv_escape(txt: str) -> str:
+    """Escapes qutoes within a text that's supposed to go into a CSV cell"""
     return '"' + txt.replace('"', '""') + '"'
 
-def gdansk_route_names():
-    req = requests.get("https://ckan.multimediagdansk.pl/dataset/c24aa637-3619-4dc2-a171-a23eec8f2172/resource/22313c56-5acf-41c7-a5fd-dc5dc72b3851/download/routes.json")
+
+def gdansk_route_names() -> dict[str, str]:
+    """Returns a mapping from route_short_name to route_long_name for ZTM Gdańsk,
+    as route_long_names aren't included in the main GTFS."""
+    req = requests.get("https://ckan.multimediagdansk.pl/dataset/c24aa637-3619-4dc2-a171-a23eec8f2172/resource/22313c56-5acf-41c7-a5fd-dc5dc72b3851/download/routes.json")  # noqa
     req.raise_for_status()
     all_routes = req.json()
 
-    route_names = {"F5": "Żabi Kruk - Westerplatte - Brzeźno", "F6": "Targ Rybny - Sobieszewo"}
+    route_names: dict[str, str] = {
+        "F5": "Żabi Kruk - Westerplatte - Brzeźno",
+        "F6": "Targ Rybny - Sobieszewo"}
 
     for routes in map(lambda i: all_routes[i]["routes"], sorted(all_routes.keys())):
         for route in routes:
@@ -51,40 +41,45 @@ def gdansk_route_names():
 
     return route_names
 
-def route_color(agency, traction):
+
+def route_color(agency: str, traction: str) -> tuple[str, str]:
+    """Generate route_color and route_text_color given an agency and route_type"""
     # Colors from mzkzg.org map
+
+    # ZTM Gdańsk
     if agency == "1":
         if traction in {"0", "900"}:
-            return "D4151D", "FFFFFF"
+            return "D4151D", "FFFFFF"  # Tram
         elif traction == {"4", "1200"}:
-            return "6CF1FA", "000000"
+            return "6CF1FA", "000000"  # Ferry
         else:
-            return "FC7DAB", "000000"
+            return "FC7DAB", "000000"  # Bus
 
+    # ZKM Gdańsk
     else:
         if traction in {"11", "800"}:
-            return "91BE40", "000000"
+            return "91BE40", "000000"  # Trolleybus
         else:
-            return "009CDA", "FFFFFF"
+            return "009CDA", "FFFFFF"  # Bus
 
 
 class TristarGtfs:
-    def __init__(self, publisher_name=None, publisher_url=None):
-        self.data_download = None
-        self.publisher_name = publisher_name
-        self.publisher_url = publisher_url
+    def __init__(self, publisher_name: str = "", publisher_url: str = ""):
+        self.publisher_name: str = publisher_name
+        self.publisher_url: str = publisher_url
+        self.data_download: datetime
 
-        self.gdansk = None
-        self.gdynia = None
+        self.gdansk: zipfile.ZipFile
+        self.gdynia: zipfile.ZipFile
 
         self.gdansk_file = TemporaryFile()
         self.gdynia_file = TemporaryFile()
 
         # self.stop_merge_table = {}
 
-        self.active_services = set()
-        self.active_shapes = set()
-        self.active_trips = set()
+        self.active_services: set[str] = set()
+        self.active_shapes: set[str] = set()
+        self.active_trips: set[str] = set()
 
         self.download()
 
@@ -92,7 +87,7 @@ class TristarGtfs:
         print("\033[1A\033[K" + "Downloading Gdansk GTFS")
         self.data_download = datetime.today()
 
-        req = requests.get("https://ckan.multimediagdansk.pl/dataset/c24aa637-3619-4dc2-a171-a23eec8f2172/resource/30e783e4-2bec-4a7d-bb22-ee3e3b26ca96/download/gtfsgoogle.zip")
+        req = requests.get("https://ckan.multimediagdansk.pl/dataset/c24aa637-3619-4dc2-a171-a23eec8f2172/resource/30e783e4-2bec-4a7d-bb22-ee3e3b26ca96/download/gtfsgoogle.zip")  # noqa
         req.raise_for_status()
         self.gdansk_file.write(req.content)
         self.gdansk_file.seek(0)
@@ -110,67 +105,76 @@ class TristarGtfs:
         version = self.data_download.strftime("%Y-%m-%d %H:%M:%S")
 
         # Agency
-        file = open("gtfs/agency.txt", mode="w", encoding="utf8", newline="\r\n")
-        file.write('agency_id,agency_name,agency_url,agency_timezone,agency_lang,agency_phone,agency_fare_url\n')
-        file.write('1,"ZTM Gdańsk","https://ztm.gda.pl/",Europe/Warsaw,pl,+48 58 520 57 75,"https://ztm.gda.pl/hmvc/index.php/test/wiecej/taryfa"\n')
-        file.write('2,"ZKM Gdynia","https://zkmgdynia.pl/",Europe/Warsaw,pl,+48 695 174 194,"https://zkmgdynia.pl/bilety-jednorazowe-zkm-w-gdyni-i-metropolitalne-mzkzg"\n')
-        file.close()
+        f = open("gtfs/agency.txt", mode="w", encoding="utf8", newline="")
+        w = csv.writer(f)
+        w.writerow([
+            "agency_id", "agency_name", "agency_url", "agency_timezone",
+            "agency_lang", "agency_phone", "agency_fare_url"])
+        w.writerow([
+            "1", "ZTM Gdańsk", "https://ztm.gda.pl/", "Europe/Warsaw",
+            "pl", "+48 58 52 44 500", "https://ztm.gda.pl/bilety/ceny-biletow,a,13"])
+        w.writerow([
+            "2", "ZKM Gdynia", "https://zkmgdynia.pl/", "Europe/Warsaw",
+            "pl", "+48 801 174 194",
+            "https://zkmgdynia.pl/bilety-jednorazowe-zkm-w-gdyni-i-metropolitalne-mzkzg"])
+        f.close()
 
         # Feed Info
         if self.publisher_name and self.publisher_url:
-            file = open("gtfs/feed_info.txt", mode="w", encoding="utf8", newline="\r\n")
-            file.write("feed_publisher_name,feed_publisher_url,feed_lang,feed_version\n")
-            file.write(f"{csv_escape(self.publisher_name)},{csv_escape(self.publisher_url)},"
-                       f"pl,{version}\n")
-            file.close()
+            f = open("gtfs/feed_info.txt", mode="w", encoding="utf8", newline="")
+            w = csv.writer(f)
+            w.writerow(["feed_publisher_name", "feed_publisher_url", "feed_lang", "feed_version"])
+            w.writerow([self.publisher_name, self.publisher_url, "pl", version])
+            f.close()
 
         # Attributions
-        file = open("gtfs/attributions.txt", mode="w", encoding="utf8", newline="\r\n")
-        file.write("attribution_id,agency_id,organization_name,is_producer,is_operator,"
-                       "is_authority,is_data_source,attribution_url\n")
-
-        file.write(f'1,1,"Based on data by: Zarząd Transportu Miejskiego w Gdańsku '
-                   f'(retrieved {version})",0,0,1,1,"http://www.ztm.gda.pl/otwarty_ztm"\n')
-
-        file.write(f'2,2,"Based on data by: Zarząd Dróg i Zieleni w Gdyni (retrieved {version}, '
-                   'agency not responsible for this dataset)",0,0,1,1,'
-                    '"http://otwartedane.gdynia.pl/pl/dataset/informacje-o-rozkladach-jazdy-i-lokalizacji-przystankow"\n')
-
-        file.close()
+        f = open("gtfs/attributions.txt", mode="w", encoding="utf8", newline="")
+        w = csv.writer(f)
+        w.writerow(["attribution_id", "agency_id", "organization_name", "is_producer",
+                    "is_operator", "is_authority", "is_data_source", "attribution_url"])
+        w.writerow([
+            "1", "1",
+            f"Based on data by: Zarząd Transportu Miejskiego w Gdańsku (retrieved {version})",
+            "0", "1", "1", "1", "https://ckan.multimediagdansk.pl/dataset/tristar"])
+        w.writerow([
+            "2", "2",
+            f"Based on data by: Zarząd Dróg i Zieleni w Gdyni (retrieved {version})",
+            "0", "0", "1", "1", "http://otwartedane.gdynia.pl/pl/dataset/informacje-o-rozkladach-jazdy-i-lokalizacji-przystankow"])  # noqa
+        f.close()
 
     @staticmethod
     def compress(target="gtfs.zip"):
         print("\033[1A\033[K" + "Compressing to " + target)
         with zipfile.ZipFile(target, mode="w", compression=zipfile.ZIP_DEFLATED) as arch:
-            for file in os.listdir("gtfs"):
-                if not file.endswith(".txt"): continue
-                arch.write(os.path.join("gtfs", file), file)
+            for f in filter(lambda i: i.name.endswith(".txt"), os.scandir("gtfs")):
+                arch.write(f.path, f.name)
 
     def merge_stops(self):
         file = open("gtfs/stops.txt", mode="w", encoding="utf-8", newline="")
-        writer = csv.DictWriter(file, ["stop_id", "stop_name", "stop_lat", "stop_lon"], extrasaction="ignore")
+        writer = csv.DictWriter(file, ["stop_id", "stop_name", "stop_lat", "stop_lon"],
+                                extrasaction="ignore")
         writer.writeheader()
 
         # Load merge table
         # The merge table maps to some non-existing stops and generally causes problems
 
-        #print("\033[1A\033[K" + "Loading stop merge table")
+        # print("\033[1A\033[K" + "Loading stop merge table")
 
-        #req = requests.get("https://ckan.multimediagdansk.pl/dataset/c24aa637-3619-4dc2-a171-a23eec8f2172/resource/f8a5bedb-7925-40c9-8d66-dbbc830939b1/download/przystanki_wspolnegda_gdy.csv")
-        #req.raise_for_status()
-        #req.encoding = "utf-8"
+        # req = requests.get("https://ckan.multimediagdansk.pl/dataset/c24aa637-3619-4dc2-a171-a23eec8f2172/resource/f8a5bedb-7925-40c9-8d66-dbbc830939b1/download/przystanki_wspolnegda_gdy.csv")  # noqa
+        # req.raise_for_status()
+        # req.encoding = "utf-8"
 
-        #for row in csv.DictReader(io.StringIO(req.text)):
+        # for row in csv.DictReader(io.StringIO(req.text)):
         #    source, target = None, None
 
         #    if row["mapped_organization_id"] == "2":
-        #        if int(row["mapped_gmv_short_name"]) < 30000: source = str(30000 + int(row["mapped_gmv_short_name"]))
+        #        if int(row["mapped_gmv_short_name"]) < 30000: source = str(30000 + int(row["mapped_gmv_short_name"]))  # noqa
         #        else: source = row["mapped_gmv_short_name"]
         #    else:
         #        source = row["mapped_gmv_short_name"]
 
         #    if row["main_organization_id"] == "2":
-        #        if int(row["main_gmv_short_name"]) < 30000: target = str(30000 + int(row["main_gmv_short_name"]))
+        #        if int(row["main_gmv_short_name"]) < 30000: target = str(30000 + int(row["main_gmv_short_name"]))  # noqa
         #        else: target = row["main_gmv_short_name"]
         #    else:
         #        target = row["main_gmv_short_name"]
@@ -182,7 +186,7 @@ class TristarGtfs:
         with self.gdansk.open("stops.txt") as buffer:
             reader = csv.DictReader(io.TextIOWrapper(buffer, encoding="utf-8", newline=""))
             for row in reader:
-                #if row["stop_id"] in self.stop_merge_table: continue
+                # if row["stop_id"] in self.stop_merge_table: continue
 
                 # Strip Gdynia from stop names — that's how it's printed on maps, see mzkzg.org
                 if row["stop_name"].startswith("Gdynia"):
@@ -195,10 +199,10 @@ class TristarGtfs:
         with self.gdynia.open("stops.txt") as buffer:
             reader = csv.DictReader(io.TextIOWrapper(buffer, encoding="utf-8", newline=""))
             for row in reader:
-                #if int(row["stop_id"]) < 30000: source = str(30000 + int(row["stop_id"]))
-                #else: source = row["stop_id"]
+                # if int(row["stop_id"]) < 30000: source = str(30000 + int(row["stop_id"]))
+                # else: source = row["stop_id"]
 
-                #if row["stop_id"] in self.stop_merge_table: continue
+                # if row["stop_id"] in self.stop_merge_table: continue
                 writer.writerow(row)
 
         file.close()
@@ -228,7 +232,8 @@ class TristarGtfs:
                 row["route_short_name"] = row["route_short_name"].strip()
 
                 row["route_long_name"] = route_names.get(row["route_short_name"], "")
-                row["route_color"], row["route_text_color"] = route_color(row["agency_id"], row["route_type"])
+                row["route_color"], row["route_text_color"] = route_color(row["agency_id"],
+                                                                          row["route_type"])
 
                 writer.writerow(row)
 
@@ -243,66 +248,66 @@ class TristarGtfs:
                 row["route_short_name"] = row["route_short_name"].strip()
                 row["route_long_name"] = row["route_long_name"].replace('""', '"')
 
-                row["route_color"], row["route_text_color"] = route_color(row["agency_id"], row["route_type"])
+                row["route_color"], row["route_text_color"] = route_color(row["agency_id"],
+                                                                          row["route_type"])
 
                 writer.writerow(row)
 
     def merge_dates(self):
         file = open("gtfs/calendar_dates.txt", mode="w", encoding="utf-8", newline="")
-        writer = csv.DictWriter(file, ["date", "service_id", "exception_type"], extrasaction="ignore")
+        writer = csv.DictWriter(file, ["date", "service_id", "exception_type"],
+                                extrasaction="ignore")
         writer.writeheader()
 
-        gdansk_dates, gdynia_dates = [], []
+        gdansk_dates: list[dict[str, Any]] = []
+        gdynia_dates: list[dict[str, Any]] = []
 
         print("\033[1A\033[K" + "Loading Gdańsk services")
 
         with self.gdansk.open("calendar_dates.txt") as buffer:
             reader = csv.DictReader(io.TextIOWrapper(buffer, encoding="utf-8", newline=""))
-            for row in reader:
-                if row["exception_type"] != "1": continue
+            for row in filter(lambda i: i["exception_type"] == "1", reader):
                 row["service_id"] = "1:" + row["service_id"]
-                row["date"] = datetime.strptime(row["date"], "%Y%m%d").date()
+                row["date"] = datetime.strptime(row["date"], "%Y%m%d").date()  # type: ignore
                 gdansk_dates.append(row)
 
         print("\033[1A\033[K" + "Loading Gdynia services")
 
         with self.gdynia.open("calendar_dates.txt") as buffer:
             reader = csv.DictReader(io.TextIOWrapper(buffer, encoding="utf-8", newline=""))
-            for row in reader:
-
-                if row["exception_type"] != "1": continue
-
+            for row in filter(lambda i: i["exception_type"] == "1", reader):
                 row["service_id"] = "2:" + row["service_id"]
-                row["date"] = datetime.strptime(row["date"], "%Y%m%d").date()
+                row["date"] = datetime.strptime(row["date"], "%Y%m%d").date()  # type: ignore
                 gdynia_dates.append(row)
 
         print("\033[1A\033[K" + "Exporting all services")
 
         # Find common start & end date for calendars
-        start_date = max(min([i["date"] for i in gdansk_dates]), min([i["date"] for i in gdynia_dates]))
-        end_date = min(max([i["date"] for i in gdansk_dates]), max([i["date"] for i in gdynia_dates]))
+        start_date: date = max(
+            min(i["date"] for i in gdansk_dates),
+            min(i["date"] for i in gdynia_dates))
 
-        services_on_date = {}
+        end_date: date = min(
+            max(i["date"] for i in gdansk_dates),
+            max(i["date"] for i in gdynia_dates))
+
+        # Figure out which service is active on every day between start_date and end_date
+        services_on_date: dict[date, set[str]] = {}
         for service_date in chain(gdansk_dates, gdynia_dates):
-            if service_date["date"] > end_date: continue
-            if service_date["date"] < start_date: continue
+            if service_date["date"] > end_date or service_date["date"] < start_date:
+                continue
 
-            date_str = service_date["date"].strftime("%Y%m%d")
-            if date_str not in services_on_date: services_on_date[date_str] = set()
+            services_on_date.setdefault(service_date["date"], set()) \
+                            .add(service_date["service_id"])
 
-            self.active_services.add(service_date["service_id"])
-            services_on_date[date_str].add(service_date["service_id"])
-
+        # Save merged services
         while start_date <= end_date:
             date_str = start_date.strftime("%Y%m%d")
-            services = sorted(services_on_date[date_str])
 
-            for service in services:
-                writer.writerow({
-                    "date": date_str,
-                    "service_id": service,
-                    "exception_type": "1"
-                })
+            for service in sorted(services_on_date[start_date]):
+                writer.writerow(
+                    {"date": date_str, "service_id": service, "exception_type": "1"})
+                self.active_services.add(service)
 
             start_date += timedelta(days=1)
 
@@ -381,8 +386,8 @@ class TristarGtfs:
         writer = csv.DictWriter(
             file,
             ["route_id", "service_id", "trip_id", "trip_headsign",
-             "direction_id", "shape_id", "wheelchair_accessible"]
-            , extrasaction="ignore")
+             "direction_id", "shape_id", "wheelchair_accessible"],
+            extrasaction="ignore")
         writer.writeheader()
 
         print("\033[1A\033[K" + "Merging Gdańsk trips")
@@ -445,9 +450,13 @@ class TristarGtfs:
 if __name__ == "__main__":
     st = time.time()
     argprs = argparse.ArgumentParser()
-    argprs.add_argument("-o", "--output-file", default="gtfs.zip", required=False, metavar="(path)", dest="target", help="destination of the gtfs file (defualt: gtfs.zip)")
-    argprs.add_argument("-pn", "--publisher-name", required=False, metavar="NAME", dest="publisher_name", help="value of feed_publisher_name")
-    argprs.add_argument("-pu", "--publisher-url", required=False, metavar="URL", dest="publisher_url", help="value of feed_publisher_url")
+    argprs.add_argument("-o", "--output-file", default="gtfs.zip", required=False,
+                        metavar="(path)", dest="target",
+                        help="destination of the gtfs file (defualt: gtfs.zip)")
+    argprs.add_argument("-pn", "--publisher-name", required=False, metavar="NAME",
+                        dest="publisher_name", help="value of feed_publisher_name")
+    argprs.add_argument("-pu", "--publisher-url", required=False, metavar="URL",
+                        dest="publisher_url", help="value of feed_publisher_url")
 
     args = argprs.parse_args()
 
